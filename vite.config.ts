910e53c546f1b500e8b029d78834f8333865405a
la-patrona <amayaag20119@gmail.com>
2023-08-02 15:38:24 -0600

    url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ventana/'
+  base: 'https://la-patrona.github.io/ventana/'
 })
